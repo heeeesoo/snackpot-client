56ccc15f118dcaed18fd38ef5c24f54a73650a64
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2023-09-07 14:10:19 +0900

    [SNK-1] feat : footer 추가

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,8 +1,31 @@
+'use client'
+import Link from "next/link";
+import Image from "next/image";
+import { FooterLinks } from "@/constant";
+import { usePathname } from "next/navigation";
 
 const Footer = () => {
+    const pathname = usePathname();
     return (
-        <div className="h-[70px] bg-white">
-            footer
+        <div className="h-[70px] bg-white flex flex-row justify-center">
+            {
+                FooterLinks.map((footerLink) => {
+                    const isSelected = pathname == footerLink.href ? true : false;
+                    return(
+                        <Link href={footerLink.href} key={footerLink.key} className="flex flex-col items-center justify-center w-1/3">
+                            <Image 
+                                src={isSelected ? footerLink.imgActive : footerLink.img}
+                                width={24}
+                                height={24}
+                                alt="footer"
+                            />
+                            <div className={`${isSelected ? 'text-SystemBrand' : 'text-SystemGray4'} text-[12px]`}>
+                                {footerLink.text}
+                            </div>
+                        </Link>
+                    )
+                })
+            }
         </div>
     );
 };
