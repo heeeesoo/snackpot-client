960af4054d173845e7166c73bdf04011014e8bea
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2023-09-12 11:14:22 +0900

    [SNK-51] feat : exercise card, list 추가

--- src/app/exercise/page.tsx
+++ src/app/exercise/page.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import ExerciseList from "@/components/exercise/ExerciseList"
+async function getData() {
+    try {
+        const apiURL = process.env.NEXT_TEST_SERVER_URL
+        const res = await fetch(`${apiURL}/exercises`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                "Accept": "application/json",
+            },
+            cache: 'no-store'
+        });
 
-const Exercise = () => {
+        if (!res.ok) {
+          console.log('Error:', res.status, res.statusText);
+          throw new Error('Failed to fetch data');
+        }
+        
+        const data = await res.json();
+        return data;
+
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+}
+
+export default async function Exercise() {
+    const data = await getData();
+
+    console.log(data)
+
+   
     return (
-        <div>
-            Exercise
+        <div className="w-screen max-w-[500px]">
+            <ExerciseList exerciseList={data.exerciseList} />
         </div>
-    );
-};
-
-export default Exercise;
+    )
+}
