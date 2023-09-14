dd94178a5a59bf9134466496c5c6ddafd58ebf14
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2023-09-14 13:47:09 +0900

    [SNK-47] feat : image domain 설정 추가

--- next.config.js
+++ next.config.js
@@ -4,12 +4,31 @@ const withPWA = require("next-pwa");
 
 const config = {
     // ...원래 next config 파일
+    images: {
+        domains: ['kormedi.com','*'],
+    },
 }
 
 const nextConfig = withPWA({
     dest: "public",
     register: true,
     skipWaiting: true,
+    // reactStrictMode: true,
+    // images: {
+    // remotePatterns: [
+    //   {
+    //     protocol: "http",
+    //     hostname: "kormedi.com",
+    //   },
+    //   {
+    //     protocol: "http",
+    //     hostname: "localhost",
+    //   },
+    // ],
+    // },
+    // images: {
+    //     domains: ["kormedi.com"],
+    // },
 })(config);
 
 module.exports = nextConfig
--- src/app/exercise/[exerciseId]/page.tsx
+++ src/app/exercise/[exerciseId]/page.tsx
@@ -1,9 +1,77 @@
-const ExerciseId = ({ params }: { params: { exerciseId: string } }) => {
+import VideoCard from "@/components/exercise/VideoCard";
+import Image from "next/image";
+import Link from "next/link";
+interface ExerciseDataType {
+    thumbnail: string;
+    youtuberThumbnail: string;
+    effect: string;
+    videoId: string;
+    title: string;
+    youtuberName: string;
+    youtuberChannelId: string;
+    youtuberDescription: string;
+    time: number;
+    bodyPart: string;
+    level: string;
+    calory: number;
+    like: boolean;
+}
+async function getData(exerciseId : number) {
+    try {
+        const apiURL = process.env.NEXT_PUBLIC_TEST_SERVER_URL
+        console.log(`${apiURL}/exercises/${exerciseId}`)
+        const res = await fetch(`${apiURL}/exercises/${exerciseId}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                "Accept": "application/json",
+            },
+            cache: 'no-store'
+        });
+
+        if (!res.ok) {
+          console.log('Error:', res.status, res.statusText);
+          throw new Error('Failed to fetch data');
+        }
+        
+        const data = await res.json();
+        return data;
+
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+}
+
+export default async function ExerciseId({ params }: { params: { exerciseId: number } }) {
+    const data : ExerciseDataType= await getData(params.exerciseId);
+    console.log(data)
+
     return (
-        <div>
-            {params.exerciseId}
+        <div className="flex flex-col">
+            <Link href={'/'}>
+                <Image
+                src={data.thumbnail}
+                width={200}
+                height={200}
+                alt="thumbnail"
+                />
+            </Link>
+            <VideoCard />
+            {data.title}
+            {data.youtuberName}
+            {data.youtuberDescription}
+            <div>
+                {data.time}
+                {data.level}
+                {data.bodyPart}
+            </div>
+            <div>
+                {data.effect}
+            </div>
+            <div>
+                {data.bodyPart}
+            </div>
         </div>
     );
 };
-
-export default ExerciseId;
