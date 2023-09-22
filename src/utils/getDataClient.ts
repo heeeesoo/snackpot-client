85b936b78b4df30926e4dc079c13b3f43f48deb4
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2023-09-22 20:14:50 +0900

    [SNK-50] feat : client fetch 추가

---
+++ src/utils/getDataClient.ts
@@ -0,0 +1,19 @@
+export const getDataClient = async (url : string) => {
+    const apiUrl = `${process.env.NEXT_PUBLIC_SERVER_URL}${url}`;
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                "Accept": "application/json",
+                // 'Authorization': TokenStore.getState().token
+            },
+        });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      throw error;
+    }
+  };
+  
