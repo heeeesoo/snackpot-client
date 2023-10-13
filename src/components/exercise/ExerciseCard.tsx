299682b2c16d4e01831cf8df75cebb8d65725186
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2023-10-13 17:56:43 +0900

    [SNK-65] chore : medium key 수정

--- src/components/exercise/ExerciseCard.tsx
+++ src/components/exercise/ExerciseCard.tsx
@@ -35,7 +35,7 @@ const ExerciseCard = ({
     likeFilter
     // youtuberProfileImg
 } : exerciseType) => {
-    const levelList: { [key: string]: string } = {'EASY':'초급', 'MID':'중급', 'HARD':'고급'};
+    const levelList: { [key: string]: string } = {'EASY':'초급', "MEDIUM":'중급', 'HARD':'고급'};
     const bodyPartList: { [key: string]: string } = {'FULL_BODY':'전신', 'UPPER_BODY':'상체', 'LOWER_BODY':'하체', 'CORE':'코어', 'ARMS':'팔', 'LEGS':'다리', 'BACK':'등', 'CHEST':'가슴', 'SHOULDERS':'어깨'};
     const router = useRouter();
     const LikedImageLink = isLiked ? LikeActive : Like;
